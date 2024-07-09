43ed79deef66cba43ffa82e90f14d37c04f47527
PraneethSimon12 <praneethsimon2@gmail.com>
2024-07-09 15:08:57 +0530

    adc

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField(blank=True, default='')
+    owner = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name='posts',)
+
+    class Meta:
+        ordering = ['created']
